dee5c2dd80a3d8b5a8df85501c51c5f8a1273469
Michael Anokye <michaelanokyej@yahoo.com>
2019-08-24 13:24:36 -0400

    added the next button function but stuck at making the question number count properly

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,7 +1,8 @@
 // Global variables
 let numberOfQuestions = $(questions).length;
-let displayedQuestionNumber = parseInt($('.questionNumber').text());
+let displayedQuestionNumber = 1;
 let displayedScore = parseInt($('.userScore').text());
+let score = $('.userScore').text();
 let pickedQuestion;
 
 function startGame(){
@@ -11,19 +12,22 @@ function startGame(){
         $('.introMessage').remove();
         $('.generatedQuestion').css('display', 'block');
         $('#gameStatus').css('display', 'block');
-        $('.questionNumber').text(1);
+        $('.questionNumber').text(displayedQuestionNumber);
     });
 }
 
 function questionCounter(){
-    displayedQuestionNumber++; 
+  displayedQuestionNumber++;
+  $('.questionNumber').text(displayedQuestionNumber);
+  console.log("displayedQuestionNumber", displayedQuestionNumber);
 }
 
 function randomQuestion(){
     // This function generates random question number 
     let random = Math.floor(Math.random() * $(questions).length);
     pickedQuestion = questions[random];
-    return pickedQuestion;
+    // return pickedQuestion;
+    // console.log("random questions");
 }
 
 function generateQuestion(){
@@ -37,19 +41,19 @@ function generateQuestion(){
     <fieldset>
     <legend>${pickedQuestion.question}</legend>
       
-    <input type="radio" id="parrotCry" name="monster" value= "Because the parrot was crying too loud.">
-    <label class='answerOption' for="parrotCry">${pickedQuestion.answers[0]}</label><br/>
+    <input type="radio" id="answerOption1" name="" value= "${pickedQuestion.answers[0]}">
+    <label for="answerOption1">${pickedQuestion.answers[0]}</label><br/>
 
-    <input type="radio" id="wife" name="monster">
-    <label class='answerOption' for="wife">${pickedQuestion.answers[1]}</label><br/>
+    <input type="radio" id="answerOption2" name="" value= "${pickedQuestion.answers[1]}">
+    <label for="answerOption2">${pickedQuestion.answers[1]}</label><br/>
 
-    <input type="radio" id="parrotSwear" name="monster">
-    <label class='answerOption' for="parrotSwear">${pickedQuestion.answers[2]}</label><br/>
+    <input type="radio" id="answerOption3" name="" value= "${pickedQuestion.answers[2]}">
+    <label for="answerOption3">${pickedQuestion.answers[2]}</label><br/>
 
-    <input type="radio" id="theLaw" name="monster">
-    <label class='answerOption' for="theLaw">${pickedQuestion.answers[3]}</label><br/>
+    <input type="radio" id="answerOption4" name="" value= "${pickedQuestion.answers[3]}">
+    <label  for="answerOption4">${pickedQuestion.answers[3]}</label><br/>
 
-    <input class="submitButton" type="button" value="submit">
+    <input class="submitAnswerButton" type="button" value="submit">
     </fieldset>
   </form>
   </div>`
@@ -57,8 +61,9 @@ function generateQuestion(){
 } else{
     renderResults();
     restartQuiz();
-    return numberOfQuestions;
+    // return numberOfQuestions;
 }
+console.log("generate question");
 }
 
 function renderResults(){
@@ -67,7 +72,7 @@ function renderResults(){
       } else if (displayedScore < 8 && displayedScore >= 5) {
         $('div.generatedQuestion').html(`<div><h3>You are almost a scholar!</h3><img src="assets/images/almostScholar.jpg" alt="almost scholar image"/><p>You got ${displayedScore} / 10</p><p>Brush up on your history if you want to conquer the world!</p><button class="restartButton">Restart Quiz</button></div>`);
       } else {
-        $('div.generatedQuestion').html(`<div><h3>You are missing a lot of history!</h3><img src="assets/images/cryingEmoji" alt="crying emji"/><p>You got ${displayedScore} / 10</p><p>Try to study the world's history. I promise you will love it!</p><button class="restartButton">Restart Quiz</button></div>`);
+        $('div.generatedQuestion').html(`<div><h3>You are missing a lot of history!</h3><img src="assets/images/cryingEmoji.jpeg" alt="crying emji"/><p>You got ${displayedScore} / 10</p><p>Try to study the world's history. I promise you will love it!</p><button class="restartButton">Restart Quiz</button></div>`);
       }
     }
 
@@ -83,19 +88,31 @@ function restartQuiz(){
 
 function checkAnswer(){
     // This function will check the user's choice 
-    $('.questionForm').on('submit', function (event) {
+    $('div.generatedQuestion').on('click', '.submitAnswerButton', function (event) {
         event.preventDefault();
         let selected = $('input:checked');
         let answer = selected.val();
         let correctAnswer = `${pickedQuestion.correctAnswer}`;
+        // console.log(correctAnswer);
+        // console.log(selected);
+        // console.log(answer);
+       
         if (answer === correctAnswer) {
-          selected.parent().addClass('correct');
+        //   selected.parent().addClass('correct');
           correctAnswerSelected();
+            console.log("correctAnswer");
+            nextQuestion();
         } else {
-          selected.parent().addClass('wrong');
+        //   selected.parent().addClass('wrong');
           wrongAnswerSelected();
+        console.log("wrong");
+        nextQuestion();
         }
       });
+
+}
+
+function scoreCounter(){
 
 }
 
@@ -106,10 +123,17 @@ function correctAnswerSelected () {
 
 function wrongAnswerSelected () {
     let correctAnswer = `${pickedQuestion.correctAnswer}`;
-    $('.questionAnswerForm').html(`<div><p><b>Sorry, you are wrong.</b><br>the correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
+    $('div.generatedQuestion').html(`<div><p><b>Sorry, you are wrong.</b><br>the correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
   }
 
-
+  function nextQuestion(){
+    $('div.generatedQuestion').on('click', '.nextButton', function (event) {
+      // event.preventDefault();
+      generateQuestion();
+      questionCounter();
+      
+    });
+  }
 
 function createGame(){
     // function to create the game as a whole
